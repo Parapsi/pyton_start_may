8356dc676c420bf550461a738512722262f690cf
parapsi <elmanana.elbanana@gmal.com>
2023-05-09 16:19:15 +0300

    first hometask 1.4

--- main_3.py
+++ main_3.py
@@ -2,3 +2,8 @@ a = input("введие число ")
 print(str(a[0]))
 print(str(a[1]))
 print(str(a[2]))
+
+b = int(input("ввдиете второе число "))
+print("", b // 100)
+print("", (b // 10) % 10)
+print("", b % 10)

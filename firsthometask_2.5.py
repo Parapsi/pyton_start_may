4165daf4d74a0b2e171c497f924401c0f9b79440
parapsi <elmanana.elbanana@gmal.com>
2023-05-10 15:27:06 +0300

    firsthometask_2.5

---
+++ firsthometask_2.5.py
@@ -0,0 +1,5 @@
+number_1 = float(input("введите первое число "))
+number_2 = float(input("введите второе число "))
+number_3 = float(input("введите третье число "))
+print("наибольшее =", max(number_1, number_2, number_3))
+

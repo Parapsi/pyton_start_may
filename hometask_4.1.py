dfb7899b3be650f5b7b6daf847ef6aa7ced0899b
parapsi <elmanana.elbanana@gmal.com>
2023-05-18 17:39:20 +0300

    hometask_4.1

--- hometask_4.1.py
+++ hometask_4.1.py
@@ -1,6 +1,7 @@
 number = input("введите число ")
 lst = list(map(int, number))
-if sum(lst[:3:]) == sum(lst[3:]):
+lenght = len(lst)//2
+if sum(lst[:lenght:]) == sum(lst[lenght:]):
     print("число счастливое")
 else:
     print("число несчастливое")

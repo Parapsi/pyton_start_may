f8692ca158296edc7ca10e1977bc552b19244b3a
parapsi <elmanana.elbanana@gmal.com>
2023-05-23 12:34:38 +0300

    hometask_5.6

--- hometask_5.6.py
+++ hometask_5.6.py
@@ -2,3 +2,7 @@ FIVE = 5
 while True:
     number = int(input("введите число "))
     print(f"{number} x {FIVE} = ", number * FIVE)
+    print("хотите продолжить? (yes/no)")
+    answer = input()
+    if answer == "yes":
+        break

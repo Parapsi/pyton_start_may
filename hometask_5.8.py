d9fbc5036db450e2c3c6be013fe49db914e8c694
parapsi <elmanana.elbanana@gmal.com>
2023-05-23 12:20:44 +0300

    hometask_5.8

--- hometask_5.8.py
+++ hometask_5.8.py
@@ -1,6 +1,6 @@
 import random
 lst_1 = [random.randint(1, 10) for i in range(4)]
-lst_2 = []
+print(lst_1)
 for i in range(len(lst_1)):
-        lst_2.append(lst_1[i] * 2)
-print(lst_1 + lst_2)
+        lst_1.append(lst_1[i] * 2)
+print(lst_1)

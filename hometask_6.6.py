0c49cc40454b9edbe39253803d6d6592728ed8de
parapsi <elmanana.elbanana@gmal.com>
2023-05-30 17:37:37 +0300

    hometask_6.6

--- hometask_6.6.py
+++ hometask_6.6.py
@@ -1,6 +1,6 @@
 word = input("")
 x = 1
-while len(word[0:x]) / 2 * word.count(word[0:x]) != len(word) / 2:
+while len(word[0:x]) // 2 * word.count(word[0:x]) != len(word) // 2:
     x += 1
 print(word[0:x])
 

ad37d2f0a44001c5f0f84568fef9ebcf33ff455c
parapsi <elmanana.elbanana@gmal.com>
2023-07-27 18:23:35 +0300

    PythonPro_3.1

--- PythonPro_3.1.py
+++ PythonPro_3.1.py
@@ -1,7 +1,6 @@
 class WrongPrice(Exception):
-    def __init__(self, price):
+    def __init__(self):
         super().__init__()
-        self.price = price
 
     def __str__(self):
         return "Wrong price exception"
@@ -15,12 +14,12 @@ class Product:
 
     def __str__(self):
         if self.price <= 0:
-            raise WrongPrice(self.price)
+            raise WrongPrice
         return f"Product name:{self.name}, price:{self.price}, description:{self.description}"
 
 
 try:
-    b = Product("Vodka", -2, "very strong drink")
+    b = Product("Vodka", 1, "very strong drink")
     print(b)
 except WrongPrice as err:
     print(err)
